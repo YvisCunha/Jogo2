c7d27491c829ad5b7e92008f15718aa041f79c06
YvisCunha <96548973+YvisCunha@users.noreply.github.com>
2021-12-22 17:32:24 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,28 @@
+var bola;
+function setup() {
+  createCanvas(400,400);
+  bola=createSprite(200,200,30,30);
+}
+
+function draw() 
+{
+  background(30);
+  if(keyIsDown(RIGHT_ARROW)){
+    bola.x=bola.x+1;
+
+  }
+  if(keyIsDown(LEFT_ARROW)){
+    bola.x=bola.x-1;
+  }
+  if(keyIsDown(UP_ARROW)){
+    bola.y=bola.y-1;
+  }
+  if(keyIsDown(DOWN_ARROW)){
+    bola.y=bola.y+1;
+  }
+drawSprites();
+}
+
+
+
+
